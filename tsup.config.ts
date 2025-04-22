5b3fd04d30e4b3ca4572d1280745322e647a6a62
antrioe <antrioe@gmali.com>
2025-04-22 17:35:30 +0800

    init(projects): init

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,11 @@
-import { readdirSync } from 'node:fs'
-import { resolve } from 'node:path'
 import { defineConfig } from 'tsup'
-import { peerDependencies } from './package.json'
-
-const paths = readdirSync(resolve(__dirname, 'src/addons')).filter(f => f.endsWith('.ts')).map(m => `src/addons/${m}`)
 
 export default defineConfig({
   entry: [
     'src/index.ts',
     'src/class.ts',
-    ...paths,
   ],
-  clean: true,
   format: ['cjs', 'esm'],
-  external: [...Object.keys(peerDependencies)],
+  clean: true,
   dts: true,
-  minify: false,
 })
